b862773ef9422ed5e84ebc23455fc2f745f0a8c5
Thomas Gusset <thomas.gusset@netsec.co>
2023-02-17 14:44:15 +0100

    exc 1

--- Hash-OTP/Program.cs
+++ Hash-OTP/Program.cs
@@ -4,7 +4,10 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("time based One Time Password");
+            TimeBasedOnetimePassword tbotp = new TimeBasedOnetimePassword("this is a random key", 6);
+
+            Console.WriteLine("One Time Password is {0}", tbotp.Otp);
         }
     }
 }
