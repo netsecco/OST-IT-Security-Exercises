407142b25c6bf5e45f39be9737a62e38cfc42254
Thomas Gusset <thomas.gusset@netsec.co>
2023-03-02 22:11:52 +0100

    new exercise

---
+++ SymEncrypt-SecureString/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace SymEncrypt_SecureString
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("secure string");
+            SecureString secStr = new SecureString("this string must be kept secret in the memory!");
+            Console.WriteLine(secStr.Value);
+        }
+    }
+}
---
+++ SymEncrypt-SecureString/SecureString.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SymEncrypt_SecureString
+{
+    /// <summary>
+    /// a string that is encrypted in the memory
+    /// </summary>
+    public class SecureString
+    {
+        private string m_value;
+
+        public SecureString(string value)
+        {
+            Value = value;
+        }
+
+        public string Value 
+        { 
+            get
+            {
+                return m_value; 
+            }
+            set
+            { 
+                m_value = value;
+            }
+        }
+
+    }
+}
